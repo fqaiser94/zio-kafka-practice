586f4e6f33136825faae0d63fae09b61bdd5187d
fqaiser94@gmail.com <fqaiser94@gmail.com>
2020-11-14 12:10:02 -0500

    can be flaky

--- src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
+++ src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
@@ -5,7 +5,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import zio.duration.durationInt
 import zio.kafka.producer.Producer
 import zio.test.Assertion.equalTo
-import zio.test.TestAspect.timeout
+import zio.test.TestAspect.{flaky, timeout}
 import zio.test._
 import zio.test.environment.{TestClock, TestConsole, TestEnvironment}
 
@@ -17,7 +17,7 @@ object MainConsumerTest extends DefaultRunnableSpec {
         programFiber <- MainConsumer.program.fork
         _ <- Producer.produce[Any, String, String](new ProducerRecord[String, String]("items", "key1", "value1"))
         _ <- TestClock.adjust(1.seconds)
-         output1 <- TestConsole.output
+        output1 <- TestConsole.output
         _ <- TestConsole.clearOutput
         _ <- Producer.produce[Any, String, String](new ProducerRecord[String, String]("items", "key2", "value2"))
         _ <- TestClock.adjust(2.seconds)
@@ -26,7 +26,7 @@ object MainConsumerTest extends DefaultRunnableSpec {
         _ <- programFiber.interrupt
       } yield assert(output1)(equalTo(Seq("key1:value1"))) &&
         assert(output2)(equalTo(Seq("key2:value2")))
-    }.provideCustomLayer(TestConsole.silent ++ Kafka.test ++ testConsumerProducerLayer) @@ timeout(60.seconds)
+    }.provideCustomLayer(TestConsole.silent ++ Kafka.test ++ testConsumerProducerLayer) @@ flaky @@ timeout(60.seconds)
   )
 
   override def spec: Spec[TestEnvironment, TestFailure[Throwable], TestSuccess] =
