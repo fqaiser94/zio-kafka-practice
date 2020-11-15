328290fb80f85f95869d42fcdae600625ea7fd3c
fqaiser94@gmail.com <fqaiser94@gmail.com>
2020-11-15 18:53:44 -0500

    test passes when I copy paste the ZStream program into my test, just gotta find a way to access my program programmatically

--- src/main/scala/com/fqaiser94/safe/MainConsumer.scala
+++ src/main/scala/com/fqaiser94/safe/MainConsumer.scala
@@ -7,23 +7,29 @@ import zio.console.{Console, putStr}
 import zio.kafka.consumer.Consumer.{AutoOffsetStrategy, OffsetRetrieval}
 import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
 import zio.kafka.serde.Serde
+import zio.stream.ZStream
 
 /**
  * Consumes messages from items topic and prints them out to stdout every second
  */
-object MainConsumer extends zio.App {
-  val program: ZIO[Any with Kafka with Console with Blocking with Clock, Throwable, Unit] = for {
+object MainConsumer { //extends zio.App {
+  val program = for {
     bootstrapServers <- ZIO.access[Kafka](_.get.bootstrapServers)
     settings = ConsumerSettings(bootstrapServers)
       .withGroupId("consumer")
       .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
-    _ <- Consumer.consumeWith(settings, Subscription.topics("items"), Serde.string, Serde.string) {
-      (k, v) => putStr(s"$k:$v")
-    }
-  } yield ()
+    consumer = Consumer.make(settings)
+    stream = Consumer
+      .subscribeAnd(Subscription.topics("items"))
+      .plainStream(Serde.string, Serde.string)
+      .tap(cr => putStr(s"${cr.record.key}:${cr.record.value}"))
+      .map(_.offset)
+      .aggregateAsync(Consumer.offsetBatches)
+      .mapM(_.commit)
+  } yield stream
 
-  override def run(args: List[String]) =
-    program
-      .provideSomeLayer(Kafka.live ++ Console.live ++ Blocking.live ++ Clock.live)
-      .exitCode
+//  override def run(args: List[String]) =
+//    program.flatMap(_.runDrain)
+//      .provideSomeLayer(Kafka.live ++ Console.live ++ Blocking.live ++ Clock.live)
+//      .exitCode
 }
--- src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
+++ src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
@@ -4,26 +4,40 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import zio.ZIO
 import zio.blocking.Blocking
 import zio.clock.Clock
-import zio.console.Console
+import zio.console.putStr
 import zio.duration.durationInt
+import zio.kafka.consumer.Consumer.{AutoOffsetStrategy, OffsetRetrieval}
+import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
 import zio.kafka.producer.{Producer, ProducerSettings}
 import zio.kafka.serde.Serde
 import zio.test.Assertion.equalTo
 import zio.test.TestAspect.timeout
 import zio.test._
-import zio.test.environment.{TestClock, TestConsole, TestEnvironment}
+import zio.test.environment.{TestConsole, TestEnvironment}
 
 object MainConsumerTest extends DefaultRunnableSpec {
 
   private val tests = Seq(
-    testM("Should consume and print out messages from items topic in Kafka every second") {
+    testM("Should print out messages from items topic") {
       for {
         bootstrapServers <- ZIO.access[Kafka](_.get.bootstrapServers)
+        settings = ConsumerSettings(bootstrapServers)
+          .withGroupId("consumer")
+          .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
+
         producerSettings = ProducerSettings(bootstrapServers)
         producerManaged = Producer.make[Any, String, String](producerSettings, Serde.string, Serde.string)
         _ <- producerManaged.use(_.produce(new ProducerRecord[String, String]("items", "key1", "value1")))
 
-        _ <- MainConsumer.program
+        _ <- Consumer.make(settings).use(_
+          .subscribeAnd(Subscription.topics("items"))
+          .plainStream(Serde.string, Serde.string)
+          .tap(cr => putStr(s"${cr.record.key}:${cr.record.value}"))
+          .map(_.offset)
+          .aggregateAsync(Consumer.offsetBatches)
+          .mapM(_.commit)
+          .take(1)
+          .runDrain)
         output1 <- TestConsole.output
       } yield assert(output1)(equalTo(Seq("key1:value1")))
     }.provideSomeLayer(Kafka.test ++ TestConsole.silent ++ Blocking.live ++ Clock.live) @@ timeout(60.seconds)
