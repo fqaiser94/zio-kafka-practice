4b9113b5a4800fb0a53a004edb94473529ede199
fqaiser94@gmail.com <fqaiser94@gmail.com>
2020-11-15 20:42:42 -0500

    able to use my MainConsumer program now

--- src/main/scala/com/fqaiser94/safe/MainConsumer.scala
+++ src/main/scala/com/fqaiser94/safe/MainConsumer.scala
@@ -1,6 +1,6 @@
 package com.fqaiser94.safe
 
-import zio.ZIO
+import zio.{Has, ZIO, ZLayer}
 import zio.blocking.Blocking
 import zio.clock.Clock
 import zio.console.{Console, putStr}
@@ -13,20 +13,26 @@ import zio.stream.ZStream
  * Consumes messages from items topic and prints them out to stdout every second
  */
 object MainConsumer { //extends zio.App {
-  val program = for {
-    bootstrapServers <- ZIO.access[Kafka](_.get.bootstrapServers)
-    settings = ConsumerSettings(bootstrapServers)
-      .withGroupId("consumer")
-      .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
-    consumer = Consumer.make(settings)
-    stream = Consumer
+  private val consumerLayer =
+    ZLayer.fromServiceManaged((kafka: Kafka.Service) => {
+      val consumerSettings = ConsumerSettings(kafka.bootstrapServers)
+        .withGroupId("consumer")
+        .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
+      Consumer.make(consumerSettings)
+    })
+
+
+  val program = {
+    val stream = Consumer
       .subscribeAnd(Subscription.topics("items"))
       .plainStream(Serde.string, Serde.string)
       .tap(cr => putStr(s"${cr.record.key}:${cr.record.value}"))
       .map(_.offset)
       .aggregateAsync(Consumer.offsetBatches)
       .mapM(_.commit)
-  } yield stream
+
+    stream.provideSomeLayer[Kafka with Blocking with Clock with Console](consumerLayer)
+  }
 
 //  override def run(args: List[String]) =
 //    program.flatMap(_.runDrain)
--- src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
+++ src/test/scala/com/fqaiser94/safe/MainConsumerTest.scala
@@ -4,10 +4,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import zio.ZIO
 import zio.blocking.Blocking
 import zio.clock.Clock
-import zio.console.putStr
 import zio.duration.durationInt
-import zio.kafka.consumer.Consumer.{AutoOffsetStrategy, OffsetRetrieval}
-import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
 import zio.kafka.producer.{Producer, ProducerSettings}
 import zio.kafka.serde.Serde
 import zio.test.Assertion.equalTo
@@ -21,23 +18,11 @@ object MainConsumerTest extends DefaultRunnableSpec {
     testM("Should print out messages from items topic") {
       for {
         bootstrapServers <- ZIO.access[Kafka](_.get.bootstrapServers)
-        settings = ConsumerSettings(bootstrapServers)
-          .withGroupId("consumer")
-          .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
-
         producerSettings = ProducerSettings(bootstrapServers)
         producerManaged = Producer.make[Any, String, String](producerSettings, Serde.string, Serde.string)
         _ <- producerManaged.use(_.produce(new ProducerRecord[String, String]("items", "key1", "value1")))
 
-        _ <- Consumer.make(settings).use(_
-          .subscribeAnd(Subscription.topics("items"))
-          .plainStream(Serde.string, Serde.string)
-          .tap(cr => putStr(s"${cr.record.key}:${cr.record.value}"))
-          .map(_.offset)
-          .aggregateAsync(Consumer.offsetBatches)
-          .mapM(_.commit)
-          .take(1)
-          .runDrain)
+        _ <- MainConsumer.program.take(1).runDrain
         output1 <- TestConsole.output
       } yield assert(output1)(equalTo(Seq("key1:value1")))
     }.provideSomeLayer(Kafka.test ++ TestConsole.silent ++ Blocking.live ++ Clock.live) @@ timeout(60.seconds)
